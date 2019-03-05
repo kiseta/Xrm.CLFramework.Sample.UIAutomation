30f8cd636bce5855b73f1906e3e5ca4f203292cc
kiseta <kiseta@gmail.com>
2019-03-04 21:05:38 -0800

    first commit

---
+++ Xrm.CLFramework.Sample.UIAutomation/CreateContactTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xrm.CLFramework.Sample.UIAutomation
+{
+	[TestClass]
+	public class CreateContactTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
